0127e7468a10142a7c44dc1e21f9a193956da626
jcwise98 <jcwise@udel.edu>
2019-03-27 13:31:49 -0400

    Jake Commit Testing

--- src/Animation4Thread.java
+++ src/Animation4Thread.java
@@ -74,7 +74,7 @@ public class Animation4Thread extends JFrame {
 	}
     
 	public static void main(String[] args) {
-		System.out.println("testing git push - derek");
+		System.out.println("testing git push - jake");
 		EventQueue.invokeLater(new Runnable(){
 			public void run(){
 				Animation4Thread a = new Animation4Thread();
